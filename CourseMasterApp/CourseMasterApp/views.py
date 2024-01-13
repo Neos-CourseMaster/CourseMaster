672896e3036b35d109bbb1f0186650fb47288d3d
zafergunayonalan <zafergunay7@gmail.com>
2024-01-14 00:45:08 +0300

    Create course slug

--- CourseMasterApp/CourseMasterApp/views.py
+++ CourseMasterApp/CourseMasterApp/views.py
@@ -74,3 +74,7 @@ def SEARCH_COURSE(request):
     }
     
     return render(request,"search/search.html",content)
+
+
+def COURSE_DETAILS(request,slug):
+    return render(request,"course/course_details.html")
