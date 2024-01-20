86c487a4d153e893620e27413a87841c133d42ea
merveonln <merveonalan2@gmail.com>
2024-01-20 15:41:10 +0300

    Verify Payment Page success/fail

--- CourseMasterApp/CourseMasterApp/settings.py
+++ CourseMasterApp/CourseMasterApp/settings.py
@@ -112,9 +112,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Istanbul'
+LANGUAGE_CODE = 'tr-TR'
 
 USE_I18N = True
 
